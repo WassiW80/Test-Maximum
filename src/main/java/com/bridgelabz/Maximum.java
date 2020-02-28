6121c9c71c1eef282c91341be65c59b5043057c9
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-28 19:29:41 +0530

    REFACTORED: Class to take in 3 variables of generic type.

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,13 +1,27 @@
 package com.bridgelabz;
 
-public class Maximum {
-    public <E extends Comparable<E>> E getMaximum(E num1, E num2, E num3) {
-        E max = num1;
-        if (num2.compareTo(max) > 0)
-            max = num2;
-        if (num3.compareTo(max) > 0)
-            max = num3;
+public class Maximum<E extends Comparable<E>> {
+    E firstValue;
+    E secondValue;
+    E thirdValue;
+
+    public Maximum(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public static <E extends Comparable<E>> E getMaximum(E firstValue, E secondValue, E thirdValue) {
+        E max = firstValue;
+        if (secondValue.compareTo(max) > 0)
+            max = secondValue;
+        if (thirdValue.compareTo(max) > 0)
+            max = thirdValue;
         return max;
 
+    }
+
+    public E getMaximum() {
+        return getMaximum(firstValue, secondValue, thirdValue);
     }
 }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -6,64 +6,46 @@ import org.junit.Test;
 public class MaximumTest {
     @Test
     public void givenIntegerFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        int result = maximum.getMaximum(80, 10, 7);
-        Assert.assertEquals(80, result);
+      Assert.assertEquals(80, new Maximum(80, 10, 7).getMaximum());
     }
 
     @Test
     public void givenIntegerSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        int result = maximum.getMaximum(10, 80, 7);
-        Assert.assertEquals(80, result);
+        Assert.assertEquals(80, new Maximum(10, 80, 7).getMaximum());
     }
 
     @Test
     public void givenIntegerThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        int result = maximum.getMaximum(7, 10, 80);
-        Assert.assertEquals(80, result);
+        Assert.assertEquals(80, new Maximum(7, 10, 80).getMaximum());
     }
 
     @Test
     public void givenFloatFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        float result = maximum.getMaximum(5.1f, 3.2f, 1.9f);
-        Assert.assertEquals(5.1f, result, 0.1);
+        Assert.assertEquals(5.1f, new Maximum(5.1f, 3.2f, 1.9f).getMaximum());
     }
 
     @Test
     public void givenFloatSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        float result = maximum.getMaximum(6.1f, 9.1f, 4.5f);
-        Assert.assertEquals(9.1f, result, 0.1);
+        Assert.assertEquals(9.1f,new Maximum(6.1f, 9.1f, 4.5f).getMaximum());
     }
 
     @Test
     public void givenFloatThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        float result = maximum.getMaximum(5.7f, 3.4f, 10.1f);
-        Assert.assertEquals(10.1f, result, 0.1);
+        Assert.assertEquals(10.1f, new Maximum(5.7f, 3.4f, 10.1f).getMaximum());
     }
 
     @Test
     public void givenStringFirstGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        String result = maximum.getMaximum("Apple", "Peach", "Banana");
-        Assert.assertEquals("Peach", result);
+        Assert.assertEquals("Peach", new Maximum("Apple", "Peach", "Banana").getMaximum());
     }
 
     @Test
     public void givenStringSecondGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        String result = maximum.getMaximum("Peach", "Apple", "Banana");
-        Assert.assertEquals("Peach", result);
+        Assert.assertEquals("Peach", new Maximum("Peach", "Apple", "Banana").getMaximum());
     }
 
     @Test
     public void givenStringThirdGreater_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum = new Maximum();
-        String result = maximum.getMaximum("Banana", "Apple", "Peach");
-        Assert.assertEquals("Peach", result);
+        Assert.assertEquals("Peach", new Maximum("Banana", "Apple", "Peach").getMaximum());
     }
 }
