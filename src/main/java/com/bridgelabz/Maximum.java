f9268a6e522a03648b53071dbfae0cb7f7e05ba0
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-28 15:57:50 +0530

    ADDED: Function for find maximum among there Float Number.

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -10,4 +10,13 @@ public class Maximum {
         return max;
 
     }
+
+    public float getMaximum(Float num1, Float num2, Float num3) {
+        Float max=num1;
+        if (num2.compareTo(max)>0)
+            max=num2;
+        if (num3.compareTo(max)>0)
+            max=num3;
+        return max;
+    }
 }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -5,23 +5,44 @@ import org.junit.Test;
 
 public class MaximumTest {
     @Test
-    public void givenFirstNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenIntegerFirstNumber_WhenLargest_ShouldReturnTrue() {
         Maximum maximum=new Maximum();
         int result = maximum.getMaximum(80, 10, 7);
         Assert.assertEquals(80,result);
     }
 
     @Test
-    public void givenSecondNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenIntegerSecondNumber_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         int result = maximum.getMaximum(10, 80, 7);
         Assert.assertEquals(80,result);
     }
 
     @Test
-    public void givenThirdNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenIntegerThirdNumber_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         int result = maximum.getMaximum(7, 10, 80);
         Assert.assertEquals(80,result);
+    }
+
+    @Test
+    public void givenFloatFirstNumber_WhenLargest_ShouldReturnTrue() {
+        Maximum maximum = new Maximum();
+        float result = maximum.getMaximum(5.1f, 3.2f, 1.9f);
+        Assert.assertEquals(5.1f,result,0.1);
+    }
+
+    @Test
+    public void givenFloatSecondNumber_WhenLargest_ShouldReturnTrue() {
+        Maximum maximum = new Maximum();
+        float result = maximum.getMaximum(6.1f, 9.1f, 4.5f);
+        Assert.assertEquals(9.1f,result,0.1);
+    }
+
+    @Test
+    public void givenFloatThirdNumber_WhenLargest_ShouldReturnTrue() {
+        Maximum maximum = new Maximum();
+        float result = maximum.getMaximum(5.7f, 3.4f, 10.1f);
+        Assert.assertEquals(10.1f,result,0.1);
     }
 }
