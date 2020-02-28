ef5c39f6312e8bb71f04d47f872e8b54c231ff26
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-28 16:51:14 +0530

    REFACTORED: All the 3 to one generic function and finding maximum.

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,31 +1,13 @@
 package com.bridgelabz;
 
 public class Maximum {
-    public int getMaximum(Integer num1, Integer num2, Integer num3) {
-        Integer max=num1;
-        if (num2.compareTo(max)>0)
-            max=num2;
-        if (num3.compareTo(max)>0)
-            max=num3;
+    public <E extends Comparable<E>> E getMaximum(E num1, E num2, E num3) {
+        E max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
         return max;
 
-    }
-
-    public float getMaximum(Float num1, Float num2, Float num3) {
-        Float max=num1;
-        if (num2.compareTo(max)>0)
-            max=num2;
-        if (num3.compareTo(max)>0)
-            max=num3;
-        return max;
-    }
-
-    public String getMaximum(String string1, String string2, String string3) {
-        String max=string1;
-        if (string2.compareTo(max)>0)
-            max=string2;
-        if (string3.compareTo(max)>0)
-            max=string3;
-        return max;
     }
 }
--- src/test/java/com/bridgelabz/MaximumTest.java
+++ src/test/java/com/bridgelabz/MaximumTest.java
@@ -5,65 +5,65 @@ import org.junit.Test;
 
 public class MaximumTest {
     @Test
-    public void givenIntegerFirstNumber_WhenLargest_ShouldReturnTrue() {
-        Maximum maximum=new Maximum();
+    public void givenIntegerFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
+        Maximum maximum = new Maximum();
         int result = maximum.getMaximum(80, 10, 7);
-        Assert.assertEquals(80,result);
+        Assert.assertEquals(80, result);
     }
 
     @Test
-    public void givenIntegerSecondNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenIntegerSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         int result = maximum.getMaximum(10, 80, 7);
-        Assert.assertEquals(80,result);
+        Assert.assertEquals(80, result);
     }
 
     @Test
-    public void givenIntegerThirdNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenIntegerThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         int result = maximum.getMaximum(7, 10, 80);
-        Assert.assertEquals(80,result);
+        Assert.assertEquals(80, result);
     }
 
     @Test
-    public void givenFloatFirstNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenFloatFirstNumberGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         float result = maximum.getMaximum(5.1f, 3.2f, 1.9f);
-        Assert.assertEquals(5.1f,result,0.1);
+        Assert.assertEquals(5.1f, result, 0.1);
     }
 
     @Test
-    public void givenFloatSecondNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenFloatSecondNumberGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         float result = maximum.getMaximum(6.1f, 9.1f, 4.5f);
-        Assert.assertEquals(9.1f,result,0.1);
+        Assert.assertEquals(9.1f, result, 0.1);
     }
 
     @Test
-    public void givenFloatThirdNumber_WhenLargest_ShouldReturnTrue() {
+    public void givenFloatThirdNumberGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         float result = maximum.getMaximum(5.7f, 3.4f, 10.1f);
-        Assert.assertEquals(10.1f,result,0.1);
+        Assert.assertEquals(10.1f, result, 0.1);
     }
 
     @Test
-    public void givenStringFirst_WhenLargest_ShouldReturnTrue() {
+    public void givenStringFirstGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         String result = maximum.getMaximum("Apple", "Peach", "Banana");
-        Assert.assertEquals("Peach",result);
+        Assert.assertEquals("Peach", result);
     }
 
     @Test
-    public void givenStringSecond_WhenLargest_ShouldReturnTrue() {
+    public void givenStringSecondGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
         String result = maximum.getMaximum("Peach", "Apple", "Banana");
-        Assert.assertEquals("Peach",result);
+        Assert.assertEquals("Peach", result);
     }
 
     @Test
-    public void givenStringThird_WhenLargest_ShouldReturnTrue() {
+    public void givenStringThirdGreater_WhenLargest_ShouldReturnTrue() {
         Maximum maximum = new Maximum();
-        String result=maximum.getMaximum("Banana", "Apple", "Peach");
-        Assert.assertEquals("Peach",result);
+        String result = maximum.getMaximum("Banana", "Apple", "Peach");
+        Assert.assertEquals("Peach", result);
     }
 }
