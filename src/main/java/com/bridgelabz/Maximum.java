b0735a6b55312559dc7944bb4894335d606cd24d
WassiW80 <sayedwasseyat111@gmail.com>
2020-02-29 11:18:38 +0530

    ADDED: Functionality to take more 3 parameters and find maximum.

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -4,24 +4,33 @@ public class Maximum<E extends Comparable<E>> {
     E firstValue;
     E secondValue;
     E thirdValue;
+    E[] extraParamaters;
 
-    public Maximum(E firstValue, E secondValue, E thirdValue) {
+    public Maximum(E firstValue, E secondValue, E thirdValue, E ... extraParamaters) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
+        this.extraParamaters = extraParamaters;
     }
 
-    public static <E extends Comparable<E>> E getMaximum(E firstValue, E secondValue, E thirdValue) {
+    private static <E extends Comparable<E>> E getMaximum(E firstValue, E secondValue, E thirdValue,E ...extraParamaters) {
         E max = firstValue;
         if (secondValue.compareTo(max) > 0)
             max = secondValue;
         if (thirdValue.compareTo(max) > 0)
             max = thirdValue;
+        if (extraParamaters.length!=0){
+            for (E extraParamater:
+                 extraParamaters ) {
+                if (extraParamater.compareTo(max)>0){
+                    max=extraParamater;
+                }
+            }
+        }
         return max;
-
     }
 
     public E getMaximum() {
-        return getMaximum(firstValue, secondValue, thirdValue);
+        return getMaximum(firstValue, secondValue, thirdValue, extraParamaters);
     }
 }
